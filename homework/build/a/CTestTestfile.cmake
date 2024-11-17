4b1d026f1b89f00d56452c00b83c68eb014dd5d8
ye <3385055109@qq.com>
2024-11-17 15:55:09 +0800

    third_week

---
+++ homework/build/a/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ye/homework/src/a
+# Build directory: /home/ye/homework/build/a
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ homework/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ye/homework/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ye/homework/devel;/home/ye/second_week/devel;/home/ye/package/devel;/home/ye/catkin_ws/devel;/home/ye/rm_ws/devel;/opt/ros/noetic'.split(';') if '/home/ye/homework/devel;/home/ye/second_week/devel;/home/ye/package/devel;/home/ye/catkin_ws/devel;/home/ye/rm_ws/devel;/opt/ros/noetic' != '' else []
